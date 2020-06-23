d7799d33de76f96eb141ee98b167f20cc9083963
Vidya-g <g.vidyarao@gmail.com>
2020-06-23 22:22:27 +0200

    Added code for alert notification.

--- Application/app/src/main/java/com/ssns/falldetectionsystem/Helper.java
+++ Application/app/src/main/java/com/ssns/falldetectionsystem/Helper.java
@@ -1,10 +1,62 @@
 package com.ssns.falldetectionsystem;
 
-public class Helper {
+//import android.content.DialogInterface;
+//import android.widget.Toast;
+//import android.content.Context;
+//import androidx.appcompat.app.AlertDialog;
 
-    public void smsAlert(){
 
+
+public class Helper{
+
+   private void alertView( String message ) {
+//        Builder alertDialog3 = new Builder(this);
+//
+//// Setting Dialog Title
+//        alertDialog3.setTitle("Alert");
+//
+//// Setting Dialog Message
+//        alertDialog3.setMessage("Are you fine?");
+//
+//// Setting Positive Yes Button
+//        alertDialog3.setPositiveButton("YES",
+//                new DialogInterface.OnClickListener() {
+//
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        // User pressed Cancel button. Write Logic Here
+//                        Toast.makeText(getApplicationContext(),
+//                                " Thank you for your Response", Toast.LENGTH_SHORT)
+//                                .show();
+//                    }
+//                });
+//// Setting Positive Yes Btn
+//        alertDialog3.setNeutralButton("NO",
+//                new DialogInterface.OnClickListener() {
+//
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        // User pressed No button. Write Logic Here
+//                        Toast.makeText(getApplicationContext(),
+//                                "Thanks for your response, will send your location to emergency contact.", Toast.LENGTH_SHORT)
+//                                .show();
+//                    }
+//                });
+//// Setting Positive "Cancel" Btn
+//        alertDialog3.setNegativeButton("Cancel",
+//                new DialogInterface.OnClickListener() {
+//
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        // User pressed Cancel button. Write Logic Here
+//                        Toast.makeText(getApplicationContext(),
+//                                "You clicked on Cancel", Toast.LENGTH_SHORT)
+//                                .show();
+//                    }
+//                });
+//// Showing Alert Dialog
+//        alertDialog3.show();
     }
+
+
+
     public void saveContact(){
 
     }
